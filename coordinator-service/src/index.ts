0bc4a0388f20c5284b5a774d2d01e94ea52df2cf
Silas Boyd-Wickizer <silasbw@gmail.com>
2020-08-23 14:33:02 -0700

    coordinator-service optionally runs with azure blog storage

--- coordinator-service/src/index.ts
+++ coordinator-service/src/index.ts
@@ -1,43 +1,99 @@
+import yargs = require('yargs')
 import bodyParser from 'body-parser'
+import fs from 'fs'
+import { StorageSharedKeyCredential } from '@azure/storage-blob'
 
-import { initExpress } from './app'
+import { BlobChunkStorage } from './blob-chunk-storage'
+import { ChunkStorage } from './coordinator'
 import { DiskCoordinator } from './disk-coordinator'
 import { DiskChunkStorage } from './disk-chunk-storage'
+import { initExpress } from './app'
 import { logger } from './logger'
 
-const port = process.env.PORT || 8080
-const storagePath = process.env.STORAGE_PATH || './.storage'
-const dbPath = process.env.DB_PATH || './.storage/db.json'
+const argv = yargs
+    .env('COORDINATOR')
+    .option('port', {
+        default: 8080,
+        type: 'number',
+    })
+    .option('db-file', {
+        default: './.storage/db.json',
+        type: 'string',
+    })
+    .option('chunk-storage-type', {
+        choices: ['disk', 'azure'],
+        default: 'disk',
+        type: 'string',
+    })
+    .option('disk-chunk-storage-directory', {
+        default: './.storage',
+        type: 'string',
+    })
+    .option('azure-access-key-file', {
+        type: 'string',
+        describe: 'File with storage account access key',
+    })
+    .option('azure-storage-account', {
+        type: 'string',
+        describe: 'Azure storage account',
+    })
+    .option('azure-container', {
+        type: 'string',
+        describe: 'Azure container name to write contributions to',
+    }).argv
 
-const chunkStorageUrl = `http://localhost:${port}/chunks`
-const diskChunkStorage = new DiskChunkStorage({ storagePath, chunkStorageUrl })
-const coordinator = new DiskCoordinator({ dbPath })
-const app = initExpress({ coordinator, chunkStorage: diskChunkStorage })
+function main(args): void {
+    logger.info('invoked with args %o', args)
 
-//
-// For local development, etc.
-//
-app.use(bodyParser.raw())
-app.post('/chunks/:chunkId/contribution/:version', async (req, res) => {
-    const participantId = req.participantId
-    const chunkId = req.params.chunkId
-    const version = req.params.version
-    const content = req.body.toString()
-
-    logger.info(`POST /chunks/${chunkId}/contribution/${version}`)
-    try {
-        await diskChunkStorage.setChunk(
-            chunkId,
-            participantId,
-            version,
-            content,
+    let diskChunkStorage
+    let chunkStorage: ChunkStorage
+    if (args.chunkStorageType === 'disk') {
+        const chunkStorageUrl = `http://localhost:${args.port}/chunks`
+        diskChunkStorage = new DiskChunkStorage({
+            storagePath: args.diskChunkStorageDirectory,
+            chunkStorageUrl,
+        })
+        chunkStorage = diskChunkStorage
+    } else if (args.chunkStorageType === 'azure') {
+        const sharedKeyCredential = new StorageSharedKeyCredential(
+            args.azureStorageAccount,
+            fs.readFileSync(args.azureAccessKeyFile).toString(),
         )
-        res.json({ status: 'ok' })
-    } catch (err) {
-        res.status(400).json({ status: 'error', message: err.message })
+        chunkStorage = new BlobChunkStorage({
+            containerName: args.azureContainer,
+            sharedKeyCredential,
+        })
     }
-})
 
-app.listen(port, () => {
-    logger.info(`listening on ${port}`)
-})
+    const coordinator = new DiskCoordinator({ dbPath: args.dbFile })
+    const app = initExpress({ coordinator, chunkStorage })
+
+    if (args.chunkStorageType === 'disk') {
+        app.use(bodyParser.raw())
+        app.post('/chunks/:chunkId/contribution/:version', async (req, res) => {
+            const participantId = req.participantId
+            const chunkId = req.params.chunkId
+            const version = req.params.version
+            const content = req.body.toString()
+
+            logger.info(`POST /chunks/${chunkId}/contribution/${version}`)
+            try {
+                await diskChunkStorage.setChunk(
+                    chunkId,
+                    participantId,
+                    version,
+                    content,
+                )
+                res.json({ status: 'ok' })
+            } catch (err) {
+                res.status(400).json({ status: 'error', message: err.message })
+            }
+        })
+    }
+
+    app.listen(args.port, () => {
+        logger.info(`listening on ${args.port}`)
+    })
+}
+
+main(argv)
