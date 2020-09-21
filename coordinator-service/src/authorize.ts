ca6f1c288b30f559e8f020594af0f1082a2a5400
Silas Boyd-Wickizer <silasbw@gmail.com>
2020-09-21 09:01:49 -0700

    Add authorization to coordinator-service (#22)
    This augments the exiting access policy implemented by the celo and dummy
    authentication schemes.

---
+++ coordinator-service/src/authorize.ts
@@ -0,0 +1,34 @@
+import { Coordinator } from './coordinator'
+
+/**
+ * Return a middleware that authorizes requests based on `groups`.
+ *
+ * @remarks
+ * If participantId is in one of the groups specified, authorize, otherwise
+ * reject.
+ */
+export function authorize({
+    coordinator,
+    groups,
+}: {
+    coordinator: Coordinator
+    groups: string[]
+}): (req, res, next) => void {
+    return function (req, res, next): void {
+        const participantId = req.participantId
+        const ceremony = coordinator.getCeremony()
+        for (const group of groups) {
+            if (ceremony[group].includes(participantId)) {
+                next()
+                return
+            }
+        }
+
+        const err = new Error(`Not authorized for ${req.path}`)
+        res.status(403).json({
+            status: 'error',
+            message: err.message,
+        })
+        next(err)
+    }
+}
