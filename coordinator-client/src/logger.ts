795f69e96524a9172a83e4123b33c3c9a2179f11
Silas Boyd-Wickizer <silasbw@gmail.com>
2020-09-21 08:42:22 -0700

    Use env var to configure logging in coordinator-client (#19)

--- coordinator-client/src/logger.ts
+++ coordinator-client/src/logger.ts
@@ -2,8 +2,17 @@ import Pino from 'pino'
 
 const runningMocha = typeof global.it === 'function'
 
+let level
+if (process.env.LOG_LEVEL) {
+    level = process.env.LOG_LEVEL
+} else if (runningMocha) {
+    level = 'warn'
+} else {
+    level = 'info'
+}
+
 export const logger = Pino({
     name: 'coordinator-service',
     prettyPrint: true,
-    level: runningMocha ? 'warn' : 'info',
+    level,
 })
