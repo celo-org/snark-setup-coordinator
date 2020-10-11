37a2ed6f1256d4b354a56a038231c90cf3b2605e
Kobi Gurkan <kobigurk@gmail.com>
2020-10-11 21:08:36 +0300

    feat: builds binaries instead of storing them in th repo

---
+++ coordinator-client/build_binaries.sh
@@ -0,0 +1,4 @@
+docker build -t snark-setup-binaries .
+docker run --name snark-setup-binaries-container snark-setup-binaries /bin/true
+docker cp snark-setup-binaries-container:/app/powersoftau .
+docker rm snark-setup-binaries-container
