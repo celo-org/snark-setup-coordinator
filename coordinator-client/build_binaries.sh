d49030b90062cfd7dcc4c00013c0fd7ebcf5f812
Silas Boyd-Wickizer <silasbw@gmail.com>
2020-10-17 12:40:39 -0700

    Tweak build_binaries.sh: add `#!/bin/sh` and `set -e`

--- coordinator-client/build_binaries.sh
+++ coordinator-client/build_binaries.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+set -e
+
 docker build -t snark-setup-binaries .
 docker run --name snark-setup-binaries-container snark-setup-binaries /bin/true
 docker cp snark-setup-binaries-container:/app/powersoftau .
