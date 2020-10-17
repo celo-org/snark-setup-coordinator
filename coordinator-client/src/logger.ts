bf541b496025ad4d1a3a4facfc1c96275cd64a19
Silas Boyd-Wickizer <silasbw@gmail.com>
2020-10-17 12:37:54 -0700

    Add options to configure log destination in coordinator-client
    LOG_FILE specifies the path to the log destination. coordinator-client will log to
    process.stdout if the LOG_FILE is '-'.

--- coordinator-client/src/logger.ts
+++ coordinator-client/src/logger.ts
@@ -3,22 +3,29 @@ import fs from 'fs'
 
 const runningMocha = typeof global.it === 'function'
 
-let level
-if (process.env.LOG_LEVEL) {
-    level = process.env.LOG_LEVEL
-} else if (runningMocha) {
-    level = 'warn'
-} else {
-    level = 'info'
+const pinoOptions: Pino.LoggerOptions = {
+    name: 'coordinator-client',
 }
 
-export const logFile = fs.createWriteStream('snark-setup.log')
+if (process.env.LOG_LEVEL) {
+    pinoOptions.level = process.env.LOG_LEVEL
+} else if (runningMocha) {
+    pinoOptions.level = 'warn'
+} else {
+    pinoOptions.level = 'info'
+}
 
-export const logger = Pino(
-    {
-        name: 'coordinator-service',
-        prettyPrint: true,
-        level,
-    },
-    logFile,
-)
+export let logFile
+
+const logPath = process.env.LOG_FILE || 'snark-setup.log'
+if (logPath === '-') {
+    logFile = process.stdout
+} else {
+    logFile = fs.createWriteStream(logPath)
+}
+
+if (logFile.isTTY) {
+    pinoOptions.prettyPrint = true
+}
+
+export const logger = Pino(pinoOptions, logFile)
