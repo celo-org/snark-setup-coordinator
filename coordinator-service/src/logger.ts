60ce3b5ce2fce431303a4c422eff49c059a5358c
Kobi Gurkan <kobigurk@gmail.com>
2020-10-27 21:57:23 +0200

    support json logging

--- coordinator-service/src/logger.ts
+++ coordinator-service/src/logger.ts
@@ -2,8 +2,13 @@ import Pino from 'pino'
 
 const runningMocha = typeof global.it === 'function'
 
+let prettyPrint = true
+if (process.env.JSON_LOGGING) {
+    prettyPrint = false
+}
+
 export const logger = Pino({
     name: 'coordinator-service',
-    prettyPrint: true,
+    prettyPrint,
     level: runningMocha ? 'warn' : 'info',
 })
