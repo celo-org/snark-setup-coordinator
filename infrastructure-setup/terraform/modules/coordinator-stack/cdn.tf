98eb12fe3cbf87f9289f90912739cb1eda20c6e3
Conner Swann <me@connerswann.me>
2020-11-19 13:22:02 -0800

    querystring caching behavior

--- infrastructure-setup/terraform/modules/coordinator-stack/cdn.tf
+++ infrastructure-setup/terraform/modules/coordinator-stack/cdn.tf
@@ -6,10 +6,11 @@ resource "azurerm_cdn_profile" "cdn_plumo" {
 }
 
 resource "azurerm_cdn_endpoint" "cdn_plumo_endpoint" {
-  name                = "CDNPlumoStorage${var.environment}"
-  profile_name        = azurerm_cdn_profile.cdn_plumo.name
-  location            = data.azurerm_resource_group.existing.location
-  resource_group_name = data.azurerm_resource_group.existing.name
+  name                          = "CDNPlumoStorage${var.environment}"
+  profile_name                  = azurerm_cdn_profile.cdn_plumo.name
+  location                      = data.azurerm_resource_group.existing.location
+  resource_group_name           = data.azurerm_resource_group.existing.name
+  querystring_caching_behaviour = "UseQueryString"
 
   origin {
     name      = "PlumoBlobStorage"
