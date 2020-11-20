63760e48a1b96f1a0432273c75b1b1197d2c253a
Conner Swann <me@connerswann.me>
2020-11-19 23:43:11 -0800

    [Fix] added host header to cdn (#54)
    * added apparently NOT OPTIONAL host header to cdn
    
    * querystring caching behavior
    
    * Microsoft CDN

--- infrastructure-setup/terraform/modules/coordinator-stack/cdn.tf
+++ infrastructure-setup/terraform/modules/coordinator-stack/cdn.tf
@@ -2,17 +2,19 @@ resource "azurerm_cdn_profile" "cdn_plumo" {
   name                = "CDNPlumo${var.environment}"
   location            = "West US"
   resource_group_name = data.azurerm_resource_group.existing.name
-  sku                 = "Standard_Akamai"
+  sku                 = "Standard_Microsoft"
 }
 
 resource "azurerm_cdn_endpoint" "cdn_plumo_endpoint" {
-  name                = "CDNPlumoStorage${var.environment}"
-  profile_name        = azurerm_cdn_profile.cdn_plumo.name
-  location            = data.azurerm_resource_group.existing.location
-  resource_group_name = data.azurerm_resource_group.existing.name
+  name                          = "CDNPlumoStorage${var.environment}"
+  profile_name                  = azurerm_cdn_profile.cdn_plumo.name
+  location                      = data.azurerm_resource_group.existing.location
+  resource_group_name           = data.azurerm_resource_group.existing.name
+  querystring_caching_behaviour = "UseQueryString"
 
   origin {
     name      = "PlumoBlobStorage"
     host_name = azurerm_storage_account.coordinator_storage.primary_blob_host
   }
+  origin_host_header = azurerm_storage_account.coordinator_storage.primary_blob_host
 }
